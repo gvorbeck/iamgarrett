205fee4246b0aeea2c82d5f3a995ffc879f44dfd
Garrett <gvorbeck@gmail.com>
2015-01-29 17:27:47 -0500

    added quotes to site image src

--- themes/iamgarrett/field-site.php
+++ themes/iamgarrett/field-site.php
@@ -6,7 +6,7 @@
         echo '<div class="site">';
           echo '<a href="' . get_sub_field('link') . '" title="' . get_sub_field('title') . '" class="site--image-link" target="_blank">';
             $img = wp_get_attachment_image_src( get_sub_field('screenshot'), 'iamg-tiny' );
-            echo '<img src=' . $img[0] . ' alt="' . get_sub_field('title') . '" />';
+            echo '<img src="' . $img[0] . '" alt="' . get_sub_field('title') . '" />';
           echo '</a>';
           echo '<h2><a href="' . get_sub_field('link') . '" title="' . get_sub_field('title') . '" target="_blank">' . get_sub_field('title') . '</a></h2>';
           echo '<div class="site--description">' . apply_filters( 'the_content', get_sub_field('description') ) . '</div>';

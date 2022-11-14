d2085073378817d398da0a19eb5225afa05462fa
gvorbeck <gvorbeck@gmail.com>
2022-11-14 10:31:16 -0500

    spelling

--- src/strings.js
+++ src/strings.js
@@ -57,7 +57,7 @@ export const IBM_CO = 'IBM';
 export const IBM_TITLE = 'Advisory Front End Developer / Software Engineer';
 export const IBM_DURATION = '10/2015 - Current';
 export const IBM_LOCATION = 'New York, NY & Remote';
-export const IBM_DESC = "As a FED within IBM's Design Team, I work with the IBM's z/OS mainframe platform to bring a modern web experience to some of the most robust systems in the industry. Whether it is updating the look and feel of their legacy systems, working to create new services, or guiding our sales teams through an ever-updating set of processes; I use my skills to ensure a modern and beautiful interraction every time.";
+export const IBM_DESC = "As a FED within IBM's Design Team, I work with the IBM's z/OS mainframe platform to bring a modern web experience to some of the most robust systems in the industry. Whether it is updating the look and feel of their legacy systems, working to create new services, or guiding our sales teams through an ever-updating set of processes; I use my skills to ensure a modern and beautiful interaction every time.";
 
 export const NBC_CO = 'NBCUniversal';
 export const NBC_TITLE = 'Web Developer/Software Developer';

34d39cc3d6edb31b4e6d03f16a956ec2ceb3fe54
gvorbeck <gvorbeck@gmail.com>
2022-12-04 14:59:45 -0500

    Change GMCM data to modern demo info

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Article from './article';
-import portfolioImg from '../images/portfolio.png';
-import gmcmScreen from '../images/gmcm-screen.png';
-import hmcScreen from '../images/hmc-screen.png';
-import oomScreen from '../images/oom-screen.png';
-import cpgScreen from '../images/cpg-screen.png';
-import kgScreen from '../images/kg-screen.png';
+import React from "react";
+import Article from "./article";
+import portfolioImg from "../images/portfolio.png";
+import gmcmScreen from "../images/gmcm-screen.png";
+import hmcScreen from "../images/hmc-screen.png";
+import oomScreen from "../images/oom-screen.png";
+import cpgScreen from "../images/cpg-screen.png";
+import kgScreen from "../images/kg-screen.png";
 import {
   PORTFOLIO_GMCM,
   PORTFOLIO_HMC,
@@ -21,7 +21,7 @@ import {
   ARTICLE_PORTFOLIO_SUBTITLE,
   ARTICLE_PORTFOLIO_DESC,
   ARTICLE_PORTFOLIO_IMG_ALT,
-} from '../strings';
+} from "../strings";
 
 export default function portfolio() {
   const sites = [
@@ -29,78 +29,78 @@ export default function portfolio() {
       name: SITE_GMCM,
       image: gmcmScreen,
       description: PORTFOLIO_GMCM,
-      link: 'https://rpg.iamgarrett.com',
-      repo: 'https://github.com/gvorbeck/gatsby-theme-dmcm',
+      link: "https://game-master-campaign-manager.github.io/",
+      repo: "https://github.com/game-master-campaign-manager/gatsby-theme-gmcm",
     },
     {
       name: SITE_HMC,
       image: hmcScreen,
       description: PORTFOLIO_HMC,
-      link: 'http://ibm.biz/hmc-mobile',
+      link: "http://ibm.biz/hmc-mobile",
     },
     {
       name: SITE_OOM,
       image: oomScreen,
       description: PORTFOLIO_OOM,
-      link: 'https://music.iamgarrett.com',
-      repo: 'https://github.com/gvorbeck/albums',
+      link: "https://music.iamgarrett.com",
+      repo: "https://github.com/gvorbeck/albums",
     },
     {
       name: SITE_KG,
       image: kgScreen,
       description: PORTFOLIO_KG,
-      link: 'https://krishantiandgarrett.com',
-      repo: 'https://github.com/gvorbeck/wedding',
+      link: "https://krishantiandgarrett.com",
+      repo: "https://github.com/gvorbeck/wedding",
     },
     {
       name: SITE_CPZ,
       image: cpgScreen,
       description: PORTFOLIO_CPZ,
-      link: 'https://gvorbeck.github.io/container-pricing/',
-      repo: 'https://github.com/gvorbeck/container-pricing',
+      link: "https://gvorbeck.github.io/container-pricing/",
+      repo: "https://github.com/gvorbeck/container-pricing",
     },
   ];
-  const siteItems = sites.map((site) =>
+  const siteItems = sites.map((site) => (
     <li key={site.name}>
       <h2>{site.name}</h2>
-      <div className='site'>
+      <div className="site">
         <a
-          className='pic-link'
+          className="pic-link"
           href={site.link}
           title={site.name}
-          target='_blank'
-          rel='noreferrer'
+          target="_blank"
+          rel="noreferrer"
         >
-          <div className='browser-head'>
-            <div className='dots' />
-            <div className='url' />
+          <div className="browser-head">
+            <div className="dots" />
+            <div className="url" />
           </div>
           <img src={site.image} alt={site.name} />
         </a>
-        <div className='desc'>
+        <div className="desc">
           <a
-            className='text-link'
+            className="text-link"
             href={site.link}
             title={site.name}
-            rel='noreferrer'
-            target='_blank'
+            rel="noreferrer"
+            target="_blank"
           >
             {site.name}
           </a>
           <a
-            className='text-link'
+            className="text-link"
             href={site.repo}
             title={site.name}
-            rel='noreferrer'
-            target='_blank'
+            rel="noreferrer"
+            target="_blank"
           >
             GitHub
           </a>
-          <p dangerouslySetInnerHTML={{__html: site.description}} />
+          <p dangerouslySetInnerHTML={{ __html: site.description }} />
         </div>
       </div>
     </li>
-  );
+  ));
 
   return (
     <Article
@@ -112,9 +112,7 @@ export default function portfolio() {
       asideImageAlt={ARTICLE_PORTFOLIO_IMG_ALT}
       text={ARTICLE_PORTFOLIO_DESC}
     >
-      <ul>
-        {siteItems}
-      </ul>
+      <ul>{siteItems}</ul>
     </Article>
-  )
+  );
 }

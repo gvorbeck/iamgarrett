855d14a793e0b1d459fc7498d3e3d20c08741395
Garrett Vorbeck <garrett.vorbeck@anywhere.re>
2025-07-21 16:06:13 -0400

    feat: update AppFooter component to use footer tag for semantic HTML and add styling

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <p>&copy;{{ new Date().getFullYear() }} J. Garrett Vorbeck</p>
+  <footer>&copy;{{ new Date().getFullYear() }} J. Garrett Vorbeck</footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+footer {
+  margin: 2rem 2rem 0;
+  padding: 0 2rem;
+}
+</style>

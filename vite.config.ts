d9951ec96637cce9084017e4b19e0ce222cf6539
Garrett Vorbeck <garrett.vorbeck@anywhere.re>
2025-07-21 21:15:17 -0400

    Fix base path for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/iamgarrett-vue/' : '/',
+  base: '/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
